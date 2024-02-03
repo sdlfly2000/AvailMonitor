ce908d0b3a3491cdee41812c6047d2cd090e1c4e
Jay Shi <25982097@qq.com>
2024-02-03 08:26:12 +0800

    change to background service

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
-using Serilog;
-using System.Collections.Concurrent;
+﻿using Serilog;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Http.Logging;
 
 namespace AvailMonitor
 {
@@ -11,46 +8,7 @@ namespace AvailMonitor
     {
         public static void Main(string[] args)
         {
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", false, false)
-                .Build();
-
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
-
-            var targets = configuration.GetSection("Monitors");
-
-            using var clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-            using var httpClient = new HttpClient(clientHandler);
-            httpClient.Timeout = TimeSpan.FromSeconds(10);
-            var results = new ConcurrentDictionary<string, string>();
-
-            Parallel.ForEach(targets.AsEnumerable(), (target) =>
-            {
-                if (target.Value == null)
-                {
-                    return;
-                }
-
-                try
-                {
-                    var response = httpClient.GetAsync(target.Value).GetAwaiter().GetResult();
-                    results.TryAdd(target.Value, response.StatusCode.ToString());
-                }
-                catch (Exception ex)
-                {
-                    results.TryAdd(target.Value, ex.Message.Replace(",",""));
-                }
-
-            });
-
-            foreach (var result in results.AsEnumerable())
-            {
-                Log.Information("Status: {code}, Call: {uri}", result.Value, result.Key);
-            }
+            CreateDefaultHost(args).Run();
         }
 
         private static IHost CreateDefaultHost(string[] args)
--- src/Worker.cs
+++ src/Worker.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using System.Net.Http;
+using System.Collections.Concurrent;
 
 namespace AvailMonitor
 {
@@ -10,7 +10,9 @@ namespace AvailMonitor
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly IConfiguration _configuration;
 
-        private Timer _timer;
+        private Timer? _timer;
+        private IList<Task> _tasks;
+        private ConcurrentDictionary<string, string> _results;
 
         public Worker(
             IHttpClientFactory httpClientFactory,
@@ -18,16 +20,18 @@ namespace AvailMonitor
         {
             _httpClientFactory = httpClientFactory;
             _configuration = configuration;
+            _tasks = new List<Task>();
+            _results = new ConcurrentDictionary<string, string>();
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _timer = new Timer(CheckAvailability, stoppingToken, 0, 300000);
+            _timer = new Timer(OnTimeJob, stoppingToken, 0, 300000);
 
             return Task.CompletedTask;
         }
 
-        private void CheckAvailability(object? state)
+        private void OnTimeJob(object? state)
         {
             var token = state is CancellationToken cancellationToken ? cancellationToken : default;
 
@@ -38,33 +42,46 @@ namespace AvailMonitor
 
             var targets = _configuration.GetSection("Monitors");
 
-            Parallel.ForEach(
-                targets.AsEnumerable(),
-                () => _httpClientFactory.CreateClient(),
-                (target) =>
+            foreach(var target in targets.AsEnumerable())
             {
-                if (target.Value == null)
+                _tasks.Add(new Task((states) =>
                 {
-                    return;
-                }
+                    var param = states as AvailParams;
 
-                try
-                {
-                    var response = httpClient.GetAsync(target.Value).GetAwaiter().GetResult();
-                    results.TryAdd(target.Value, response.StatusCode.ToString());
-                }
-                catch (Exception ex)
-                {
-                    results.TryAdd(target.Value, ex.Message.Replace(",", ""));
-                }
+                    var response = param!.HttpClient.GetAsync(param.Target.Value).GetAwaiter().GetResult();
+                    _results.TryAdd(param.Target.Value!, response.StatusCode.ToString());
+                    param.HttpClient.Dispose();
+                },
+                new AvailParams { HttpClient = _httpClientFactory.CreateClient(), Target = target}));
+            }
 
-            });
+            foreach(var task in _tasks)
+            {
+                task.Start();
+            }
 
-            foreach (var result in results.AsEnumerable())
+            Task.WhenAll(_tasks);
+
+            foreach (var result in _results.AsEnumerable())
             {
                 Log.Information("Status: {code}, Call: {uri}", result.Value, result.Key);
             }
+        }
 
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (_timer != null)
+            {
+                _timer.Dispose();
+            }
+
+            return base.StopAsync(cancellationToken);
+        }
+
+        private class AvailParams
+        {
+            public required HttpClient HttpClient { get; set; }
+            public KeyValuePair<string, string?> Target { get; set; }
         }
     }
 }
